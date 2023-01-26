9d87b0ca5324fc6cb2541356af55a89069968634
user <annasuhanova2005@gmail.com>
2023-01-26 19:34:09 +0400

    Init
    Signed-off-by: user <annasuhanova2005@gmail.com>

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLabel
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class My_Project(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Большая задача по Maps API")
+        self.setStyleSheet("background-color: rgb(177, 179, 222)")
+        self.setGeometry(500, 500, 500, 500)
+        self.setMaximumSize(500, 500)
+        self.setMinimumSize(500, 500)
+
+        self.my_map = QLabel(self)
+        tmp = self.my_map.resize(300, 300)
+        tmp = self.my_map.move(100, 100)
+
+        self.x = 37.620070
+        self.y = 55.753630
+        self.sizexy = 300
+
+        request = f"https://static-maps.yandex.ru/1.x/?ll={self.x},{self.y}&size={self.sizexy},{self.sizexy}"
 
 
 
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = My_Project()
+    ex.show()
+    sys.exit(app.exec())
